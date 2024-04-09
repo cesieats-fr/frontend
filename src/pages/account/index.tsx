efeea39aff377fee03fb9eb511b663ad61363449
xaerose <alexandre.germonvillebellet@viacesi.fr>
2024-04-09 22:59:15 +0200

    add delete account button

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -2,9 +2,10 @@ import { Button, TextField } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../store";
 import { useState } from 'react';
-import { edit } from "../../api/services/account";
-import { setAccount } from "../../store/reducers/account";
+import { deleteAccount, edit } from "../../api/services/account";
+import { removeAccount, setAccount } from "../../store/reducers/account";
 import { IAccount } from 'cesieats-service-types/src/account';
+import { redirect } from "react-router-dom";
 
 const type = ['Client','Livreur','Restaurant'];
 function Account() {
@@ -30,20 +31,29 @@ function Account() {
         dispatch(setAccount(acc));
         
     }
+    const handleDeleteAccount = async () => {
+
+        await deleteAccount();
+        dispatch(removeAccount());
+        redirect('/');
+    }
     return (
-        <div className="w-full items-center flex flex-col">
-            <div className="w-75 items-center flex flex-col">
+        <div className="w-full h-full items-center flex flex-col">
+            <div className="w-75 h-full items-center flex flex-col">
                 <h1 className="m-5">Infos et modifications du compte</h1>
                 <div className="w-full items-center flex flex-col">
                   Type de compte : {type[account.accountType]}
                 </div>
-                <div className="w-full items-center flex flex-col">
+                <div className="w-full h-full items-center flex flex-col">
                   <TextField id="outlined-basic" label="E-mail"       variant="outlined" className="w-full" required margin="dense" value={email}     onChange={(e) => setEmail(e.target.value)}/>
                   <TextField id="outlined-basic" label="Mot de passe" variant="outlined" className="w-full" required margin="dense" value={password}  onChange={(e) => setPassword(e.target.value)} type="password" />
                   <TextField id="outlined-basic" label="Nom"          variant="outlined" className="w-full" required margin="dense" value={name}      onChange={(e) => setName(e.target.value)}/>
                   <TextField id="outlined-basic" label="Prénom"       variant="outlined" className="w-full" required margin="dense" value={forname}   onChange={(e) => setForname(e.target.value)}/>
                   <TextField id="outlined-basic" label="Adresse"      variant="outlined" className="w-full"          margin="dense" value={address}    onChange={(e) => setAddress(e.target.value)}   multiline       rows={4}/>
                   <Button variant="contained" onClick={() => { handleEditAccount() }}>Valider les modifications</Button>
+                  <div className="w-full h-full">
+                    <Button variant="contained" onClick={() => { handleDeleteAccount() }}>Supprimer le compte</Button>
+                  </div>
                 </div>
             </div>
         </div>
