1cea80a9abe94016c65e65e708a0ee2a60e48c55
Keanu <keanuharrell@hotmail.fr>
2024-04-09 15:12:48 +0200

    add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { RouterProvider } from 'react-router';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useDispatch } from 'react-redux';
-import { accountAPI } from './api';
-import { useEffect } from 'react';
-import { setAccount } from './store/reducers/account';
+// import { useDispatch } from 'react-redux';
+// import { accountAPI } from './api';
+// import { useEffect } from 'react';
+// import { setAccount } from './store/reducers/account';
 import router from './routers';
+import './index.css';
 
 const theme = createTheme({
   typography: {
@@ -17,14 +18,14 @@ const theme = createTheme({
 });
 
 function App(): React.ReactElement {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    accountAPI.login()
-      .then((response) => {
-        dispatch(setAccount(response.account));
-        localStorage.setItem('token', response.token);
-      });
-    }, [dispatch]);
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   accountAPI.login()
+  //     .then((response) => {
+  //       dispatch(setAccount(response.account));
+  //       localStorage.setItem('token', response.token);
+  //     });
+  //   }, [dispatch]);
   
     return (
       <ThemeProvider theme={theme}>
