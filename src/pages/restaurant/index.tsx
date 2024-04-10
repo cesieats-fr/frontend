85d5c5c9b592fbaa12d8397802a4822cb9b9c188
Tolgabylk <73704413+Tolgabylk@users.noreply.github.com>
2024-04-10 11:53:25 +0200

    adding restaurant view

--- src/pages/restaurant/index.tsx
+++ src/pages/restaurant/index.tsx
@@ -1,9 +1,77 @@
-function Restaurant() {
+import React, { useState } from "react";
+import {
+  ListItemText,
+  ListItemIcon,
+  ListItemButton,
+  ListItem,
+  Typography,
+  Divider,
+  List,
+  Toolbar,
+  Drawer,
+  Box,
+} from "@mui/material";
+import {
+  RestaurantMenu,
+  Home,
+  RoomService,
+  BarChart,
+} from "@mui/icons-material";
+
+const drawerWidth = 240;
+const icons = [<Home />, <RestaurantMenu />, <RoomService />, <BarChart />];
+
+export default function MenuDrawer() {
+  const [selectedMenu, setSelectedMenu] = useState("Home");
+
+  const handleMenuClick = (menu) => {
+    setSelectedMenu(menu);
+  };
+
   return (
-      <div>
-          Restaurant
-      </div>
+    <Box className="flex">
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+      >
+        <Toolbar />
+        <Box className="">
+          <List>
+            {["Home", "Menu", "Commandes", "Statistiques"].map(
+              (text, index) => (
+                <ListItem key={text} disablePadding>
+                  <ListItemButton onClick={() => handleMenuClick(text)}>
+                    <ListItemIcon>{icons[index] && icons[index]}</ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              )
+            )}
+          </List>
+          <Divider />
+        </Box>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
+        <Typography paragraph>
+          {/* Condition pour afficher les informations en fonction de l'élément de menu sélectionné */}
+          {selectedMenu === "Home" && <>Bienvenue sur la page d'accueil</>}
+          {selectedMenu === "Menu" && <>...</>}
+          {selectedMenu === "Commandes" && (
+            <>Informations pour les commandes...</>
+          )}
+          {selectedMenu === "Statistiques" && (
+            <>Informations pour les statistiques...</>
+          )}
+        </Typography>
+      </Box>
+    </Box>
   );
 }
-
-export default Restaurant;
