7f454a7c4eeb01a8664e8d3388ef16fe12b7ba8f
unknown <tolga.beylik.tb@gmail.com>
2024-04-12 00:06:34 +0200

    Removing Banner on home and correction of spelling mistakes

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,65 +1,72 @@
 import { RadioGroup, FormControlLabel, Radio } from "@mui/material";
-import { useState } from 'react';
+import { useState } from "react";
 import { EAccountType } from "../../enums";
-import RestaurantRegister from './restaurant';
-import DefaultRegister from './default';
+import RestaurantRegister from "./restaurant";
+import DefaultRegister from "./default";
 
 export interface IRegisterProps {
-    accountType: EAccountType;
+  accountType: EAccountType;
 }
 
 const accountTypes = [
-    {
-        name: "Client",
-        type: EAccountType.CLIENT
-    },
-    {
-        name: "Restaurant",
-        type: EAccountType.RESTAURANT
-    },
-    {
-        name: "Livreur",
-        type: EAccountType.DELIVER
-    },
-    {
-        name: "Service commercial",
-        type: EAccountType.SALESDEPARTEMENT
-    }
-]
+  {
+    name: "Client",
+    type: EAccountType.CLIENT,
+  },
+  {
+    name: "Restaurant",
+    type: EAccountType.RESTAURANT,
+  },
+  {
+    name: "Livreur",
+    type: EAccountType.DELIVER,
+  },
+  {
+    name: "Service commercial",
+    type: EAccountType.SALESDEPARTEMENT,
+  },
+];
 function Register() {
-    const [accountType, setAccountType] = useState<EAccountType>(EAccountType.CLIENT);
+  const [accountType, setAccountType] = useState<EAccountType>(
+    EAccountType.CLIENT
+  );
 
-    const getRegisterForm = () => {
-        switch(accountType) {
-            case EAccountType.RESTAURANT:
-                return <RestaurantRegister accountType={accountType} />;
-            default:
-                return <DefaultRegister accountType={accountType} />;
-        }
+  const getRegisterForm = () => {
+    switch (accountType) {
+      case EAccountType.RESTAURANT:
+        return <RestaurantRegister accountType={accountType} />;
+      default:
+        return <DefaultRegister accountType={accountType} />;
     }
+  };
 
-    return (
+  return (
+    <div className="w-full items-center flex flex-col">
+      <div className="w-75 items-center flex flex-col">
+        <h1 className="m-2">Créer un compte</h1>
         <div className="w-full items-center flex flex-col">
-            <div className="w-75 items-center flex flex-col">
-                <h1 className="m-2">Créer un compte</h1>
-                <div className="w-full items-center flex flex-col">
-                    <RadioGroup row aria-labelledby="row-radio-buttons-group-label" name="row-radio-buttons-group" onChange={(e) => setAccountType(parseInt(e.target.value))}>
-                        {accountTypes && accountTypes.map((aT, index) => (
-                            <FormControlLabel
-                                key={index}
-                                checked={accountType == aT.type}
-                                value={aT.type}
-                                control={<Radio />}
-                                label={aT.name}
-                                
-                            />
-                        ))}
-                    </RadioGroup>
-                    { getRegisterForm() }
-                </div>
-            </div>
+          <RadioGroup
+            row
+            aria-labelledby="row-radio-buttons-group-label"
+            name="row-radio-buttons-group"
+            onChange={(e) => setAccountType(parseInt(e.target.value))}
+          >
+            {accountTypes &&
+              accountTypes.map((aT, index) => (
+                <FormControlLabel
+                  key={index}
+                  checked={accountType == aT.type}
+                  value={aT.type}
+                  control={<Radio />}
+                  label={aT.name}
+                />
+              ))}
+          </RadioGroup>
+          {getRegisterForm()}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Register;
