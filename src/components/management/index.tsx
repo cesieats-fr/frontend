bf81b7b677cc41604dd44548b3a449c806ed72ca
Tolgabylk <73704413+Tolgabylk@users.noreply.github.com>
2024-04-12 12:18:54 +0200

    fix management

--- src/components/management/index.tsx
+++ src/components/management/index.tsx
@@ -5,20 +5,20 @@ import {
   Dialog,
   DialogTitle,
   IconButton,
-  Stack,  
-  TextField,  
+  Stack,
+  TextField,
   Typography,
 } from "@mui/material";
 import { IAccount } from "cesieats-service-types/src/account";
 
 import { Edit, Delete } from "@mui/icons-material";
-import { useDispatch, useSelector } from "react-redux";
-import { editAccountByID, removeAccount } from "../../store/reducers/account";
+import { useDispatch } from "react-redux";
+import {
+  editAccountByID,
+  deleteAccountById,
+} from "../../store/reducers/account";
 import { AppDispatch } from "../../store";
 import { useEffect, useState } from "react";
-import { deleteAccount, deleteAccountById } from "../../api/services/account";
-
-
 
 interface IAccountCardProps {
   account: IAccount;
@@ -31,43 +31,42 @@ function ClientAccountCard({ account }: IAccountCardProps) {
   const [open, setOpen] = useState(false);
 
   const handleEditClientAccount = () => {
-    setOpen(true);    
+    setOpen(true);
   };
-  
+
   useEffect(() => {
-    setEmail(account.email); 
+    setEmail(account.email);
     setName(account.name);
     setForname(account.forname);
-    setAddress(account.address || '');
-}, [account]);
+    setAddress(account.address || "");
+  }, [account]);
 
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [forname, setForname] = useState('');
-  const [address, setAddress] = useState('');
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [forname, setForname] = useState("");
+  const [address, setAddress] = useState("");
 
+  const handleValidate = () => {
+    const acc: IAccount = {
+      _id: account._id,
+      email,
+      name,
+      forname,
+      address,
+      accountType: account.accountType,
+      password: account.password,
+    };
+    editAccountByID(acc);
+    dispatch(editAccountByID(acc));
 
-  const handleValidate = () => {   
-        const acc : IAccount = {
-            _id: account._id,
-            email,
-            name,
-            forname,
-            address,
-            accountType: account.accountType,
-            password: account.password
-        }
-        dispatch(editAccountByID(acc));
-     
     setOpen(false);
   };
 
-  
   const handleDeleteClientAccount = async () => {
+    console.log(account._id);
     await deleteAccountById(account._id!);
-    dispatch(removeAccount());
-   
-}
+    dispatch(deleteAccountById(account._id!));
+  };
   return (
     <Stack>
       <Card variant="elevation">
@@ -100,12 +99,45 @@ function ClientAccountCard({ account }: IAccountCardProps) {
 
       <Dialog onClose={handleEditClientAccount} open={open}>
         <DialogTitle>Modifier un client</DialogTitle>
-        <TextField label="E-mail"       variant="outlined" className="w-full" required margin="dense" value={email}     onChange={(e) => setEmail(e.target.value)} />
-                    <TextField label="Nom"          variant="outlined" className="w-full" required margin="dense" value={name}      onChange={(e) => setName(e.target.value)}/>
-                    <TextField label="Prénom"       variant="outlined" className="w-full" required margin="dense" value={forname}   onChange={(e) => setForname(e.target.value)}/>
-                    <TextField label="Adresse"      variant="outlined" className="w-full"          margin="dense" value={address}    onChange={(e) => setAddress(e.target.value)}   multiline       rows={4}/>
-                
-      <Button onClick={handleValidate}>Valider les modifications</Button>
+        <TextField
+          label="E-mail"
+          variant="outlined"
+          className="w-full"
+          required
+          margin="dense"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          label="Nom"
+          variant="outlined"
+          className="w-full"
+          required
+          margin="dense"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <TextField
+          label="Prénom"
+          variant="outlined"
+          className="w-full"
+          required
+          margin="dense"
+          value={forname}
+          onChange={(e) => setForname(e.target.value)}
+        />
+        <TextField
+          label="Adresse"
+          variant="outlined"
+          className="w-full"
+          margin="dense"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+          multiline
+          rows={4}
+        />
+
+        <Button onClick={handleValidate}>Valider les modifications</Button>
       </Dialog>
     </Stack>
   );
