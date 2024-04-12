ec65cb17b119381c65406217c48754d58d851c08
Keanu <keanuharrell@hotmail.fr>
2024-04-12 14:21:49 +0200

    fix

--- src/pages/delivery/index.tsx
+++ src/pages/delivery/index.tsx
@@ -1,20 +1,16 @@
-import Typography from "@mui/material/Typography";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Divider, Stack } from "@mui/material";
 import DividedMenu, { IDividedMenuPart } from "../../components/dividedMenu";
 import Deliveries from "./deliveries";
 import DeliveriesList from "./deliveries/title";
 import DeliveryDriver from "./delivery";
 import DeliveryList from "./delivery/title";
-import { AppDispatch, RootState } from "../../store";
+import { AppDispatch } from "../../store";
 import { getDeliveries } from "../../store/reducers/delivery";
 import { useEffect } from "react";
 
 function OrderPage() {
   const dispatch = useDispatch<AppDispatch>();
-  const deliveries = useSelector(
-    (state: RootState) => state.delivery.deliveries
-  );
 
   useEffect(() => {
     dispatch(getDeliveries());
